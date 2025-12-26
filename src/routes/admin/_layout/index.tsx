7fec71d5141d125cb5b6838ae56866f2df8c69a4
Alexandre Chs <oliuuum@gmail.com>
2025-12-26 17:26:54 +0100

    refactor(dashboard): consistent naming

--- src/routes/admin/_layout/index.tsx
+++ src/routes/admin/_layout/index.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router'
-import DashboardPage from '@/features/admin/dashboard/view/DashboardPage'
+import DashboardPage from '@/features/admin/dashboard/DashboardPage'
 
 export const Route = createFileRoute('/admin/_layout/')({
   component: DashboardPage,
